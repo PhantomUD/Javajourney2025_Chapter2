729c454136f6db3914f6d5596bb7823c337e7f4d
PhantomUD <udeze.ayodeji@gmail.com>
2025-09-10 14:23:10 +0100

    Day 2

---
+++ Addition4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Addition4 {
+public static void main(String[] args) {
+
+	Scanner Udeze = new Scanner(System.in);
+
+	System.out.print ("First Integer");
+	int number1 = Udeze.nextInt();
+
+	System.out.print ("Second Integer");
+	int number2 = Udeze.nextInt();
+
+	int sum = number1 + number2;
+	System.out.printf ("Sum of new interger %d%n", sum);
+
+}
+}
---
+++ Addition6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Addition6 {
+public static void main(String[] args) {
+
+	Scanner Udeze = new Scanner(System.in);
+
+	System.out.print ("First integer");
+	int number1 = Udeze.nextInt();
+
+	System.out.print ("Second integer");
+	int number2 = Udeze.nextInt();
+
+	int sum = number1 + number2;
+	System.out.printf ("Sum is new Interger %d%n", sum);
+}
+}
